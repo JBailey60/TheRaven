b40c309bd98b8fb81d0781c29b35f617e384b51e
JBailey60 <43798767+JBailey60@users.noreply.github.com>
2021-03-31 17:51:19 -0400

    Init Commit

---
+++ TheRaven-Core/src/main/java/got/raven/TheRaven/TheRavenCoreApplication.java
@@ -0,0 +1,13 @@
+package got.raven.TheRaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRavenCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRavenCoreApplication.class, args);
+	}
+
+}
---
+++ TheRaven-Core/src/test/java/got/raven/TheRaven/TheRavenCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package got.raven.TheRaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRavenCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
